7508ba3a08cf5f2910c68a8c0cd7f1a7a97c2e11
Pavel Stojka <pavel.stojka@student.ukf.sk>
2019-11-24 19:22:48 +0100

    TP01-52 Upraviť ikonky v admin rozhraní

--- devel/resources/views/admin/partials/sidebar.blade.php
+++ devel/resources/views/admin/partials/sidebar.blade.php
@@ -2,14 +2,14 @@
     <section class="sidebar">
         <ul class="sidebar-menu" data-widget="tree">
             <li class="header">Erazmus Program</li>
-            <li class="{{ request()->is('dashboard') ? 'active' : '' }}"><a href="{{ url('/dashboard') }}"><i class="fa fa-dashboard "></i> <span>Dashboard</span></a></li>
-            <li class="{{ request()->is('dashboard/school') ? 'active' : '' }}"><a href="{{ url('/dashboard/school') }}"><i class="fa fa-dashboard "></i> <span>Školy</span></a></li>
-            <li class="{{ request()->is('dashboard/mobility') ? 'active' : '' }}"><a href="{{ url('/dashboard/mobility') }}"><i class="fa fa-dashboard "></i> <span>Mobility</span></a></li>
-            <li class="{{ request()->is('dashboard/feedback') ? 'active' : '' }}"><a href="{{ url('/dashboard/feedback') }}"><i class="fa fa-dashboard "></i> <span>Feedback</span></a></li>
+            <li class="{{ request()->is('dashboard') ? 'active' : '' }}"><a href="{{ url('/dashboard') }}"><i class="fa fa-bar-chart "></i> <span>Dashboard</span></a></li>
+            <li class="{{ request()->is('dashboard/school') ? 'active' : '' }}"><a href="{{ url('/dashboard/school') }}"><i class="fa fa-graduation-cap "></i> <span>Školy</span></a></li>
+            <li class="{{ request()->is('dashboard/mobility') ? 'active' : '' }}"><a href="{{ url('/dashboard/mobility') }}"><i class="fa fa-map-signs "></i> <span>Mobility</span></a></li>
+            <li class="{{ request()->is('dashboard/feedback') ? 'active' : '' }}"><a href="{{ url('/dashboard/feedback') }}"><i class="fa fa-bullhorn "></i> <span>Feedback</span></a></li>
 
             <li class="header">Manažment</li>
-            <li class="{{ request()->is('dashboard/profile') ? 'active' : '' }}"><a href="{{ url('/dashboard/profile') }}"><i class="fa fa-gears"></i><i class="fa fa-circle-o text-aqua pull-right"></i><span>Profil</span></a></li>
-            <li class="{{ request()->is('dashboard/logs') ? 'active' : '' }}"><a href="{{ url('/dashboard/logs') }}"><i class="fa fa-leaf"></i><i class="fa fa-circle-o text-orange pull-right"></i><span>Logy</span></a></li>
+            <li class="{{ request()->is('dashboard/profile') ? 'active' : '' }}"><a href="{{ url('/dashboard/profile') }}"><i class="fa fa-user"></i><i class="fa fa-circle-o text-aqua pull-right"></i><span>Profil</span></a></li>
+            <li class="{{ request()->is('dashboard/logs') ? 'active' : '' }}"><a href="{{ url('/dashboard/logs') }}"><i class="fa fa-file-text-o"></i><i class="fa fa-circle-o text-orange pull-right"></i><span>Logy</span></a></li>
         </ul>
     </section>
 </aside>
