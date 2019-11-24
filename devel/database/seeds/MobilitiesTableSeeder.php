998eca7708c2ef6cb454be481891ac17eebd7ad9
patriktariska <pattar19@icloud.com>
2019-11-24 19:44:25 +0100

    Uprava mobility seederu

--- devel/database/seeds/MobilitiesTableSeeder.php
+++ devel/database/seeds/MobilitiesTableSeeder.php
@@ -14,10 +14,10 @@ class MobilitiesTableSeeder extends Seeder
     {
         DB::table('mobilities')->delete();
         Mobility::create([
-            'type' => 'erazmus',
+            'type' => 'Erasmus+',
             'desc' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.']);
         Mobility::create([
-            'type' => 'typ mobility 2',
+            'type' => 'CEEPUS',
             'desc' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.']);
 
     }
