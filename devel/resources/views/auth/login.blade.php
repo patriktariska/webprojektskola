00df31f62c826008fd9f5bf84fcd3918b519588d
patriktariska <pattar19@icloud.com>
2019-12-13 21:29:08 +0100

    TP01-58 Login CSS

--- devel/resources/views/auth/login.blade.php
+++ devel/resources/views/auth/login.blade.php
@@ -1,65 +1,64 @@
-@extends('layouts.app')
+@extends('public.layouts.default')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-
-                <div class="panel-body">
+<div class="site-section bg-light">
+    <div class="container p-4 bg-white border shadow">
+        <div class="row">
+                <div class="col-md-7">
+                    <h4>Prihlásenie študenta do systému</h4>
+                </div>
+                <div class="col-md-5">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
-
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">E-Mail adresa</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
                                 @endif
                             </div>
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4 control-label">Heslo</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
                                 @endif
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-12 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Zapamätať
                                     </label>
+                                    <button type="submit" class="btn btn-primary float-right">
+                                        Prihlásiť
+                                    </button>
+                                    <div>
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            Zabudnuté heslo?
+                                        </a>
+                                        <a href="{{ route('register') }}" class="btn btn-link">Registrácia</a>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
-        </div>
     </div>
 </div>
 @endsection
